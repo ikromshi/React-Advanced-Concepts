4c6439d8da161da178309a88b013c521fd865e32
Ikrom Numonov <numanovikrom454@gmail.com>
2022-10-18 21:29:20 -0400

    GET and POST with axios

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,46 @@
 import './App.css';
-import api from "./api/posts";
-import { useEffect, useState } from 'react';
+import { useEffect, useMemo, useState } from 'react';
+import axios from "axios";
+
+const api = axios.create({
+  baseURL: "http://localhost:3500/posts"
+})
+
+type Posts = {
+  title: string;
+  body: string;
+}[];
 
 function App() {
-  const [posts, setPosts] = useState([]);
+  const [posts, setPosts] = useState<Posts>();
 
   useEffect(() => {
-    const fetchPosts = async () => {
-      try {
-        const response = await api.get("/posts");
-        setPosts(response.data);
-      } catch (error) {
-        console.log(error)
-      }
-    }
+    getPosts();
+  }, []);
 
-    fetchPosts();
-  }, [])  
-
+  const getPosts = async () => {
+    const data = await api.get("/").then(res => {
+      return res.data;
+    });
+    console.log(data);
+    setPosts(data);
+  }
+  
+  // Posting data
+  const createPosts = async () => {
+    const response = await api.post("/", {
+      id: 5, title: "Number five", datetime: "Right now", body: "Posting data with axios"
+    });
+    getPosts();
+  };
+  
+  
   return (
     <div className="app">
       <header className="App-header">
-
+      <button onClick={createPosts}>Add a post</button>
+      <button onClick={getPosts}>Get Posts</button>
+      {posts?.map((post, idx) => <h3 key={idx}>Title: {post.title} </h3>)}
       </header>
     </div>
   )
@@ -31,3 +50,14 @@ export default App;
 
 // axios creates the json on its own - no need to do response.json()
 // axios catches errors outside the 200 code status - no need to do if (!response.ok) .... 
+// 
+
+// const getPosts = useMemo(() => (
+//   async () => {
+//    const data = await api.get("/").then(res => {
+//      return res.data;
+//    });
+//    console.log(data);
+//    setPosts(data);
+//   }
+// ), [posts])
